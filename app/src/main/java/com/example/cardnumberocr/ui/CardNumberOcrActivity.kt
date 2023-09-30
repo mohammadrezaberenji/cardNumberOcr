3e269cf5b02050c45f1ae4ca0530b036c29c2409
mohammadReza Berenji <berenji.mr@gmail.com>
2023-09-30 15:01:05 +0330

    first init

---
+++ app/src/main/java/com/example/cardnumberocr/ui/CardNumberOcrActivity.kt
@@ -0,0 +1,31 @@
+package com.example.cardnumberocr.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import com.example.cardnumberocr.databinding.ActivityCardNumberOcrBinding
+
+class CardNumberOcrActivity : ComponentActivity() {
+
+    private val TAG = CardNumberOcrActivity::class.java.simpleName
+
+    private var _binding: ActivityCardNumberOcrBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate: ")
+        super.onCreate(savedInstanceState)
+        _binding = ActivityCardNumberOcrBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onDestroy() {
+        Log.i(TAG, "onDestroy: ")
+        _binding = null
+        super.onDestroy()
+    }
+
+
+}
+
