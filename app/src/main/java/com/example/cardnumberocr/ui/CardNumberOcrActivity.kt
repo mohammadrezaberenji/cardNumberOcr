ccf811a936837b1b8148f13977dafcce5f03d931
mohammadReza Berenji <berenji.mr@gmail.com>
2023-10-01 15:57:18 +0330

    add rectangle for ocr input

--- app/src/main/java/com/example/cardnumberocr/ui/CardNumberOcrActivity.kt
+++ app/src/main/java/com/example/cardnumberocr/ui/CardNumberOcrActivity.kt
@@ -2,8 +2,15 @@ package com.example.cardnumberocr.ui
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PixelFormat
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
+import android.util.Size
+import android.view.SurfaceHolder
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.Camera
@@ -12,19 +19,17 @@ import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
-import androidx.camera.core.impl.CameraConfig
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.lifecycleScope
 import com.example.cardnumberocr.databinding.ActivityCardNumberOcrBinding
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import java.util.concurrent.Executors
 
+
 @ExperimentalGetImage
-class CardNumberOcrActivity : ComponentActivity() {
+class CardNumberOcrActivity : ComponentActivity(), SurfaceHolder.Callback {
 
     private val TAG = CardNumberOcrActivity::class.java.simpleName
 
@@ -34,10 +39,12 @@ class CardNumberOcrActivity : ComponentActivity() {
 
     private lateinit var mCameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private var camera: Camera? = null
-    private var imageAnalysis:ImageAnalysis?=null
+    private var imageAnalysis: ImageAnalysis? = null
+    var boxWidth = 0
+    var boxHeight = 0
 
     private val extractDataUseCase by lazy {
-        ExtractDataUseCase(TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS))
+        ExtractDataUseCase(TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS), this)
     }
 
     private var requestCameraPermissionLauncher =
@@ -53,10 +60,12 @@ class CardNumberOcrActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i(TAG, "onCreate: ")
         super.onCreate(savedInstanceState)
+        window
         _binding = ActivityCardNumberOcrBinding.inflate(layoutInflater)
         setContentView(binding.root)
         checkCameraPermission()
         prepareCameraConfig()
+        initSurfaceView()
     }
 
     private fun prepareCameraConfig() {
@@ -65,28 +74,32 @@ class CardNumberOcrActivity : ComponentActivity() {
 
         mCameraProviderFuture = ProcessCameraProvider.getInstance(this)
         mCameraProviderFuture.addListener({
-                val cameraSelector =
-                    CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build()
-                val preview = Preview.Builder().build().apply {
-                    setSurfaceProvider(binding.preview.surfaceProvider)
-                }
-                imageAnalysis = ImageAnalysis.Builder()
-                    .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_YUV_420_888)
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+            val preview = Preview.Builder().build().apply {
+                setSurfaceProvider(binding.preview.surfaceProvider)
+            }
 
-                imageAnalysis?.setAnalyzer(Executors.newSingleThreadExecutor()) {
-                    analyze(it)
-                }
+            imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(Size(720, 1488))
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_YUV_420_888)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
 
-                if (camera != null) {
-                    Log.d(TAG, "initView: camera is null then going to unbind")
-                    cameraProviderFuture.get().unbindAll()
-                }
+            imageAnalysis?.setAnalyzer(Executors.newSingleThreadExecutor()) {
+                analyze(it)
+            }
 
-                camera = cameraProviderFuture.get()
-                    .bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
+            if (camera != null) {
+                Log.d(TAG, "initView: camera is null then going to unbind")
+                cameraProviderFuture.get().unbindAll()
+            }
+
+            camera = cameraProviderFuture.get()
+                .bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
+
         }, ContextCompat.getMainExecutor(this))
+
     }
 
 
@@ -123,6 +136,71 @@ class CardNumberOcrActivity : ComponentActivity() {
         super.onDestroy()
     }
 
+    private fun initSurfaceView() {
+        Log.i(TAG, "initSurfaceView: ")
+        val surfaceView = binding.overLay
+        surfaceView.setZOrderOnTop(true);
+        val holder = surfaceView.getHolder();
+        holder.setFormat(PixelFormat.TRANSPARENT);
+        holder.addCallback(this)
 
+
+    }
+
+    private fun drawFocusRect(color: Int, holder: SurfaceHolder) {
+        Log.i(TAG, "drawFocusRect: ")
+        val displaymetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displaymetrics)
+        val heigh = binding.preview.getHeight()
+        val width = binding.preview.getWidth()
+        val cameraHeight = heigh
+        val cameraWidth = width
+
+
+        val left: Int
+        val right: Int
+        val top: Int
+        val bottom: Int
+        var diameter: Int
+        diameter = width
+
+        if (heigh < width) {
+            diameter = heigh
+        }
+        val offset = (0.05 * diameter).toInt()
+        diameter -= offset
+        val canvas = holder.lockCanvas()
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR)
+        //border's properties
+        val paint = Paint()
+        paint.setStyle(Paint.Style.STROKE)
+        paint.setColor(color)
+        paint.setStrokeWidth(5F)
+        left = width / 2 - diameter / 3
+        top = heigh / 2 - diameter / 3
+        right = width / 2 + diameter / 3
+        bottom = heigh / 2 + diameter / 3
+        val xOffset = left
+        val Offset = top
+
+        boxHeight = bottom - top
+        boxWidth = right - left
+
+        //Changing the value of x in diameter/x will change the size of the box ; inversely proportionate to x
+        canvas.drawRect(left.toFloat(), top.toFloat(), right.toFloat(), bottom.toFloat(), paint)
+        holder.unlockCanvasAndPost(canvas)
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+        drawFocusRect(Color.parseColor("#b3dabb"), p0);
+
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+    }
 }
 
--- app/src/main/java/com/example/cardnumberocr/ui/ExtractDataUseCase.kt
+++ app/src/main/java/com/example/cardnumberocr/ui/ExtractDataUseCase.kt
@@ -1,17 +1,88 @@
 package com.example.cardnumberocr.ui
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.DisplayMetrics
+import android.util.Log
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognizer
 
+
 @ExperimentalGetImage
-class ExtractDataUseCase(private val textRecognizer: TextRecognizer) {
+class ExtractDataUseCase(private val textRecognizer: TextRecognizer, private val context: Context) {
     private val TAG = "ExtractDataUseCase"
-    fun process(imageProxy: ImageProxy, onSuccess:(String)->Unit, onFailure:(Exception)->Unit) {
-        if (imageProxy.image==null) return
-        val imageInput = InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
-        val p = textRecognizer.process(imageInput)
+    fun process(
+        imageProxy: ImageProxy,
+        onSuccess: (String) -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        if (imageProxy.image == null) return
+        val imageInput =
+            InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+
+        val bimap = imageProxy.toBitmap()
+
+
+        Log.i(TAG, "process: ")
+
+        val displayMetrics = DisplayMetrics()
+
+        val activity = context as CardNumberOcrActivity
+
+        activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val height = bimap.height
+        val width = bimap.width
+
+        var left = 0
+        var right = 0
+        var top = 0
+        var bottom = 0
+        var diameter = 0
+
+        diameter = width;
+
+        val offset = (0.05 * diameter)
+        diameter -= offset.toInt()
+
+
+
+        left = width / 2 - diameter / 3;
+        top = height / 2 - diameter / 3;
+        right = width / 2 + diameter / 3;
+        bottom = height / 2 + diameter / 3;
+
+        Log.i(TAG, "process: image width : ${imageProxy.image!!.width}")
+        Log.i(TAG, "process: image height : ${imageProxy.image!!.height}")
+
+        Log.i(TAG, "process: left : $left")
+        Log.i(TAG, "process: top : $top")
+        Log.i(TAG, "process: right : $right")
+        Log.i(TAG, "process:  bottom : $bottom")
+
+        Log.i(TAG, "process: bitmap width : ${bimap.width}")
+        Log.i(TAG, "process: bitmap height : ${bimap.height}")
+
+        Log.i(TAG, "process: box width : ${activity.boxWidth}")
+        Log.i(TAG, "process: box height : ${activity.boxHeight}")
+
+
+        val newBitMap = Bitmap.createBitmap(bimap, left, top, activity.boxWidth, activity.boxHeight)
+
+        Log.i(TAG, "process: new bitmap width : ${newBitMap.width}")
+        Log.i(TAG, "process: new bitmap height ; ${newBitMap.height}")
+
+
+        val p = textRecognizer.process(
+            InputImage.fromBitmap(
+                newBitMap,
+                imageProxy.imageInfo.rotationDegrees
+            )
+        )
         p.addOnSuccessListener {
             onSuccess.invoke(it.text)
         }
