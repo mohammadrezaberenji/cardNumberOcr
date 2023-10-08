85c6f98a71e96fad27cc6f06b035fbab248b9b63
HamidReza <hrk1380@gmail.com>
2023-10-08 00:49:27 -0800

    fix bug

--- app/src/main/java/com/example/cardnumberocr/ui/MainActivity.kt
+++ app/src/main/java/com/example/cardnumberocr/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.cardnumberocr.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.camera.core.ExperimentalGetImage
 import com.example.cardnumberocr.R
+import com.tools.cardnumberocr.CardNumberOcrActivity
 
 class MainActivity:ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @ExperimentalGetImage override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread.sleep(500)
+        startActivity(Intent(this,CardNumberOcrActivity::class.java))
     }
 }
